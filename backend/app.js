820d107424226d647233ab487c0eb2c888460fd1
soeren.metje <soeren.metje@stud.uni-goettingen.de>
2020-03-26 20:22:20 +0100

    added output to backend updated return codes

--- backend/app.js
+++ backend/app.js
@@ -20,20 +20,22 @@ router.use(bodyParser.json());
 // ==========================================================================
 
 router.post('/login', (req, res) => {
+    console.log('Request /login');
     if(!req.body.name || !req.body.password) {
         res.status(401).send('missing name or password');
         return;
     }
     db.selectByName(req.body.name, (err, user) => {
         if (err) return res.status(500).send('Error on the server.');
-        if (!user) return res.status(404).send('No user found.');
+        if (!user) return res.status(401).send('No user found.');
         let passwordIsValid = bcrypt.compareSync(req.body.password, user.user_pass);
         if (!passwordIsValid)
-            return res.status(401).send({auth: false, token: null});
+            return res.status(401).json({auth: false, token: null});
         let token = jwt.sign({id: user.id}, secret, {
             expiresIn: 86400 // expires in 24 hours
         });
-        res.status(200).send({auth: true, token: token});
+        console.log('valid credentials');
+        res.status(200).json({auth: true, token: token, user: req.body.name});
     });
 });
 
